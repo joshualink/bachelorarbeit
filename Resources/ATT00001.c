ca2ac64b522b28f92e3c2a6ead99b68078fbaa17
Joshua <uwdws@student.kit.edu>
2017-08-17 16:58:21 +0200

    added all chapters

---
+++ Resources/ATT00001.c
@@ -0,0 +1,22 @@
+
+_________________________________
+Karlsruher Institut für Technologie (KIT)
+Institut für Anthropomatik und Robotik - IAR
+Humanoids and Intelligence Systems Lab
+ 
+Pascal Meißner
+Wiss. Mitarbeiter
+ 
+Adenauerring 4 (Gebäude 50.21, Raum 206)
+76131 Karlsruhe
+ 
+Telefon: +49 721 608 - 45942
+Fax: +49 721 608 - 45959
+E-Mail: pascal.meissner@kit.edu
+ 
+his.anthropomatik.kit.edu
+ 
+KIT – Die Forschungsuniversität in der Helmholtz-Gemeinschaft
+
+Das KIT ist seit 2010 als familiengerechte Hochschule zertifiziert
+
